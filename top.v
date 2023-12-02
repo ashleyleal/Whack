64603fcbaeeddf7c86630db309fb1eb7f4494a99
chocoboash <anleal2004@gmail.com>
2023-12-02 12:52:37 -0500

    removed garbage and added vga adapters

--- top.v
+++ top.v
@@ -1,14 +1,27 @@
-module Top (CLOCK_50, KEY, SW, LEDR, VGA_X, VGA_Y, VGA_COLOR, plot);
+module Top (CLOCK_50, KEY, SW, LEDR, 		
+    VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B)   						//	VGA Blue[9:0]);
+    
     input  wire         CLOCK_50;   // DE-series 50 MHz clock signal
     input  wire [ 3: 0] KEY;        // DE-series pushbuttons
     input  wire [ 9: 0] SW;         // DE-series SW switches
     output wire [ 9: 0] LEDR;       // DE-series LEDs
-    output wire [ 7: 0] VGA_X;      // "VGA" column
-    output wire [ 6: 0] VGA_Y;      // "VGA" row
-    output wire [ 2: 0] VGA_COLOR;  // "VGA pixel" colour (0-7)
-    output wire         plot;       // "Pixel" is drawn when this is pulsed
-
-    // Wires for the VGA module
+	  output			VGA_CLK;   				//	VGA Clock
+	  output			VGA_HS;					//	VGA H_SYNC
+	  output			VGA_VS;					//	VGA V_SYNC
+	  output			VGA_BLANK_N;				//	VGA BLANK
+	  output			VGA_SYNC_N;				//	VGA SYNC
+	  output	[7:0]	VGA_R;   				//	VGA Red[7:0] Changed from 10 to 8-bit DAC
+	  output	[7:0]	VGA_G;	 				//	VGA Green[7:0]
+	  output	[7:0]	VGA_B;   				//	VGA Blue[7:0]
+    
+    // Wires for the draw module
     wire         [ 7: 0] x;
     wire         [ 6: 0] y;
     wire         [ 2: 0] color;
@@ -34,6 +47,27 @@ module Top (CLOCK_50, KEY, SW, LEDR, VGA_X, VGA_Y, VGA_COLOR, plot);
     // Module instantiations
     GameFSM gameFSM(.clk(CLOCK_50), .reset(reset), .input_signal(KEY[1]), .control_signal(KEY[2]), .timer_signal(KEY[3]), .state(state));
     drawImage drawImage(.iResetn(reset), .iClock(CLOCK_50), .iState(state), .oX(x), .oY(y), .oColour(color), .oPlot(plot));
+
+    vga_adapter VGA(
+			.resetn(reset),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(plot),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 4;
+		defparam VGA.BACKGROUND_IMAGE = "whackstartsceen.mif";
 
     // Using DESIM VGA Adapter, need to change to one for board later
 
