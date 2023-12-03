ab84e94666697e7f4997ab2e0de9361b8aca77f3
AndyTe <89328581+AndyTe@users.noreply.github.com>
2023-12-03 03:30:40 -0500

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -7,7 +7,7 @@ module datapath(
 	 input player_signal,  // player input hit or miss
 	 output reg enable_control, // switch back from mole to game screen
 	 output reg timer_done,  //switch to end game
-	 output reg timer_doneTimer,  //timer end
+	 output reg timer_doneGame; // game done
 	 output reg game_start,  // game started
     output reg [7:0] data_result, // result of data
 	 output reg wren, // read write
@@ -22,12 +22,11 @@ module datapath(
 		data_result = 8'd0;
 		data_in = 8'd0;
 		counter = 27'd0;
-		enable_control = 1'd0;
-		timer_done = 1'd0;
-		timer_doneTime = 1'd0;
-		flag = 1'd0;
-		game_start = 1'd0;
-		wren = 1'd0;
+		enable_control = 1'b0;
+		timer_done = 1'b0;
+		timer_doneGame <= 1'b0;
+		game_start = 1'b0;
+		wren = 1'b0;
 		address =5'd0;
 	 end
 	 
@@ -36,12 +35,11 @@ module datapath(
 				data_result <= 8'd0;
 				data_in <= 8'd0;
 				counter <= 27'd0;
-				enable_control <= 1'd0;
-				timer_done <= 1'd0;
-				timer_doneTime <= 1'd0;
-				flag <= 1'd0;
-				game_start <= 1'd0;
-				wren <= 1'd0;
+				enable_control <= 1'b0;
+				timer_done <= 1'b0;
+				timer_doneGame <= 1'b0;
+				game_start <= 1'b0;
+				wren <= 1'b0;
 				address <=5'd0;
 		  end
 	  else
@@ -51,21 +49,20 @@ module datapath(
 					data_result <= 8'd0;
 					data_in <= 8'd0;
 			  		counter <= 27'd0;
-					enable_control <= 1'd0;
-					timer_done <= 1'd0;
-					timer_doneTime <= 1'd0;
-					flag <= 1'd0;
-					game_start <= 1'd0;
-					wren <= 1'd0;
+					enable_control <= 1'b0;
+					timer_done <= 1'b0;
+			 		timer_doneGame <= 1'b0;
+					game_start <= 1'b0;
+					wren <= 1'b0;
 				3'b001:
-					game_start <= 1'd1;
+					game_start <= 1'b1;
 					
 					if (counter <= 27'd50000000) begin
-						enable_control <= 1'd1;
+						enable_control <= 1'b1;
 						counter <= 27'd0;
 					end
 					else begin
-						enable_control <= 1'd0;
+						enable_control <= 1'b0;
 						counter <= counter + 1;
 					end
 				3'b010:
@@ -73,13 +70,13 @@ module datapath(
 						data_in <= data_in + 1;
 					end
 			  		if (counter <= 27'd50000000) begin
-						enable_control <= 1'd1;
-						wren <= 1'd0
+						enable_control <= 1'b1;
+						wren <= 1'b0
 						counter <= 27'd0;
 					end
 					else begin
-						enable_control <= 1'd0;
-						wren <= 1'd1;
+						enable_control <= 1'b0;
+						wren <= 1'b1;
 						counter <= counter + 1;
 					end
 				
@@ -88,13 +85,13 @@ module datapath(
 						data_in <= data_in + 1;
 					end
 					if (counter <= 27'd50000000) begin
-						enable_control <= 1'd1;
-						wren <= 1'd0
+						enable_control <= 1'b1;
+						wren <= 1'b0
 						counter <= 27'd0;
 					end
 			  		else begin
-						enable_control <= 1'd0;
-						wren <= 1'd1;
+						enable_control <= 1'b0;
+						wren <= 1'b1;
 						counter <= counter + 1;
 					end
 				
@@ -103,13 +100,13 @@ module datapath(
 						data_in <= data_in + 1;
 					end
 					if (counter <= 27'd50000000) begin
-						enable_control <= 1'd1;
-						wren <= 1'd0
+						enable_control <= 1'b1;
+						wren <= 1'b0
 						counter <= 27'd0;
 					end
 					else begin
-						enable_control <= 1'd0;
-						wren <= 1'd1;
+						enable_control <= 1'b0;
+						wren <= 1'b1;
 						counter <= counter + 1;
 					end
 				
@@ -118,24 +115,24 @@ module datapath(
 						data_in <= data_in + 1;
 					end
 					if (counter <= 27'd50000000) begin
-						enable_control <= 1'd1;
-						wren <= 1'd0
+						enable_control <= 1'b1;
+						wren <= 1'b0
 						counter <= 27'd0;
 					end
 					else begin
-						enable_control <= 1'd0;
-						wren <= 1'd1;
+						enable_control <= 1'b0;
+						wren <= 1'b1;
 						counter <= counter + 1;
 					end
 				
 				3'b110:
 					if (counter <= 27'd50000000) begin
-						timer_doneTime <= 1d'1
+						timer_doneGame <= 1'b1
 						address <= address + 1;
 						counter <= 27'd0;
 					end
 					else begin
-						timer_doneTime <= 1'd0;
+						timer_doneGame <= 1'b0;
 						counter <= counter + 1;
 					end
 		  end case
