719678f9b34ceed7e041c56358a99cdfafd488f2
AndyTe <89328581+AndyTe@users.noreply.github.com>
2023-12-04 10:44:31 -0500

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,146 +1,138 @@
-module datapath(
-    input clk, 
+module Datapath(
+    input clk, // one sec clock
     input Reset,
     input [7:0] data_in, //player score
-    input state, //state of game
+    input [2:0] state, //state of game
 	 input timer_signal, // from game timer signal
 	 input player_signal,  // player input hit or miss
 	 output reg enable_control, // switch back from mole to game screen
 	 output reg timer_done,  //switch to end game
-	 output reg timer_doneGame; // game done
 	 output reg game_start,  // game started
     output reg [7:0] data_result, // result of data
 	 output reg wren, // read write
 	 output reg [4:0] address // address
     );
 
-   	 reg [7:0] data_in; 
-	 reg [27:0] counter; // toggle
-
+	 reg flag; // toggle
+	 
 	
 	 initial begin
-		data_result = 8'd0;
-		data_in = 8'd0;
-		counter = 27'd0;
+		data_result = 8'b0;
+		flag = 1'b0;
 		enable_control = 1'b0;
 		timer_done = 1'b0;
-		timer_doneGame <= 1'b0;
 		game_start = 1'b0;
 		wren = 1'b0;
-		address =5'd0;
+		address =5'b0;
 	 end
 	 
     always@(posedge clk) begin
         if (Reset) begin
-				data_result <= 8'd0;
-				data_in <= 8'd0;
-				counter <= 27'd0;
+				data_result <= 8'b0;
+				flag <= 1'b0;
 				enable_control <= 1'b0;
 				timer_done <= 1'b0;
-				timer_doneGame <= 1'b0;
 				game_start <= 1'b0;
 				wren <= 1'b0;
-				address <=5'd0;
+				address <=5'b0;
 		  end
 	  else begin
 		  
 		  case (state)
-				3'b000:
-					data_result <= 8'd0;
-					data_in <= 8'd0;
-			  		counter <= 27'd0;
+				3'b000: begin
+					data_result <= 8'b0;
+					flag <= 1'b0;
 					enable_control <= 1'b0;
 					timer_done <= 1'b0;
-			 		timer_doneGame <= 1'b0;
 					game_start <= 1'b0;
 					wren <= 1'b0;
-				3'b001:
+				end
+				3'b001: begin
 					game_start <= 1'b1;
 					
-					if (counter <= 27'd50000000) begin
+					if (flag == 1'b1) begin
 						enable_control <= 1'b1;
-						counter <= 27'd0;
+						flag <= 1'b0;
 					end
 					else begin
 						enable_control <= 1'b0;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
-				3'b010:
+				end
+				3'b010: begin
 					if (player_signal) begin
-						data_in <= data_in + 1;
+						data_result <= data_in + 1;
 					end
-			  		if (counter <= 27'd50000000) begin
+			  		if (flag <= 1'b1) begin
 						enable_control <= 1'b1;
-						wren <= 1'b0
-						counter <= 27'd0;
+						wren <= 1'b0;
+						flag <= 1'b0;
 					end
 					else begin
 						enable_control <= 1'b0;
 						wren <= 1'b1;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
-				
-				3'b011:
+				end
+				3'b011: begin
 					if (player_signal) begin
-						data_in <= data_in + 1;
+						data_result <= data_in + 1;
 					end
-					if (counter <= 27'd50000000) begin
+					if (flag == 1'b1) begin
 						enable_control <= 1'b1;
-						wren <= 1'b0
-						counter <= 27'd0;
+						wren <= 1'b0;
+						flag <= 1'b0;
 					end
 			  		else begin
 						enable_control <= 1'b0;
 						wren <= 1'b1;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
-				
-				3'b100:
+				end
+				3'b100: begin
 					if (player_signal) begin
-						data_in <= data_in + 1;
+						data_result <= data_in + 1;
 					end
-					if (counter <= 27'd50000000) begin
+					if (flag == 1'b1) begin
 						enable_control <= 1'b1;
-						wren <= 1'b0
-						counter <= 27'd0;
+						wren <= 1'b0;
+						flag <= 1'b0;
 					end
 					else begin
 						enable_control <= 1'b0;
 						wren <= 1'b1;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
-				
-				3'b101:
+				end
+				3'b101: begin
 					if (player_signal) begin
-						data_in <= data_in + 1;
+						data_result <= data_in + 1;
 					end
-					if (counter <= 27'd50000000) begin
+					if (flag == 1'b1) begin
 						enable_control <= 1'b1;
-						wren <= 1'b0
-						counter <= 27'd0;
+						wren <= 1'b0;
+						flag <= 1'b0;
 					end
 					else begin
 						enable_control <= 1'b0;
 						wren <= 1'b1;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
-				
-				3'b110:
-					if (counter <= 27'd50000000) begin
-						timer_doneGame <= 1'b1
+				end
+				3'b110: begin
+					if (flag == 1'b1) begin	
 						address <= address + 1;
-						counter <= 27'd0;
+						flag <= 1'b0;
 					end
 					else begin
-						timer_doneGame <= 1'b0;
-						counter <= counter + 1;
+						flag <= 1'b1;
 					end
+				end
 		  endcase
 	   end 
+		
 		  if (timer_signal) begin
 				timer_done <= 1;
 		  end
     end
-	 
-	 assign data_result = data_in;
 endmodule
