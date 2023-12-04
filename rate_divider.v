2aa384a4f16d7bebdededc5c9409a9e666899b44
AndyTe <89328581+AndyTe@users.noreply.github.com>
2023-12-04 07:42:54 -0500

    Update rate_divider.v

--- rate_divider.v
+++ rate_divider.v
@@ -1,25 +1,29 @@
 module rate_divider (
-    input ClockIn,
+    input Clk,
     input Reset,
-    output Enable
+    output reg Enable
 );
-
+	
   reg [26:0] counter;
-
-  always @(posedge ClockIn) begin
+  
+  initial begin
+		counter <= 27'b010111110101111000010000000;
+  end
+  
+  always @(posedge Clk) begin
     if (Reset) begin
-		counter <= 27d'50000000;
-		Enable <= 1'd0;
+		counter <= 27'b010111110101111000010000000;
+		Enable <= 1'b0;
     end
 
     else begin
-		if (counter == 0) begin 
-			counter <= 27'd50000000;
-			Enable <= 1'd1;
+		if (counter == 27'b0) begin 
+			counter <= 27'b010111110101111000010000000;
+			Enable <= 1'b1;
 		end 
 		else begin
 			counter <= counter - 1;
-			Enable <= 1'd0;
+			Enable <= 1'b0;
 		end
 	end
   end
