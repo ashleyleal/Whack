2abff0f7cd24bee7c46b5170c25035df89a7aa02
AndyTe <89328581+AndyTe@users.noreply.github.com>
2023-12-06 01:43:53 -0500

    Update Crossing.v

--- Crossing.v
+++ Crossing.v
@@ -5,24 +5,33 @@ module Crossing (
   input [7:0] data_in,
   output reg [7:0] data_out
 );
-  reg [7:0] data;
+  reg [7:0] data_temp;
+  reg [7:0] data_temp1;
   
   always @(posedge clk) begin
     if (reset) begin
-      data <= 0;
+      data_temp <= 0;
     end 
 	 
 	 else begin
-		data <= data_in;
+		data_temp <= data_in;
     end
   end
-  
+  always @(posedge clk2) begin
+    if (reset) begin
+		data_temp1 <= 0;
+	 end
+	 else begin
+		data_temp1 <= data_temp;
+	 end
+	end
+
   always @(posedge clk2) begin
     if (reset) begin
 		data_out <= 0;
 	 end
 	 else begin
-		data_out <= data;
+		data_out <= data_temp1;
 	 end
 	end
 endmodule
